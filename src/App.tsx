411be6d8861c7ca5a0618139ec1a307b3eb5bd6e
Tallyson Freitas <tallyson.eu@outlook.com>
2023-11-28 13:55:06 -0300

    commit inicial comercio online

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ContainerPrincipal, EstiloGlobal } from './style'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <ContainerPrincipal>
+        <h1>ola mundo</h1>
+      </ContainerPrincipal>
+    </>
+  )
+}
+
+export default App
---
+++ src/style.ts
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const ContainerPrincipal = styled.div`
+  max-width: 1920px;
+  width: 100%;
+  margin: 0 auto;
+  background-color: #f44;
+`
+
+export const EstiloGlobal = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+`
