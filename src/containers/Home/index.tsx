ba7d6a6b8a2608f45deccd87af85bd8c25d6694a
Tallyson Freitas <tallyson.eu@outlook.com>
2023-12-06 15:07:09 -0300

    Primeira sessao

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,5 +1,14 @@
 import { Link } from 'react-router-dom'
-import { SHeader, SNavBar } from './style'
+import {
+  SBotaoContact,
+  SContexto,
+  SHeader,
+  SImagePerfil,
+  SNavBar,
+  SSessaoHome,
+  STextoDestaque,
+  STituloDestaque
+} from './style'
 
 const Home = () => {
   return (
@@ -18,6 +27,24 @@ const Home = () => {
           </li>
         </SNavBar>
       </SHeader>
+      <SSessaoHome>
+        <div>
+          <STextoDestaque>ALUNO FRONT-END</STextoDestaque>
+          <STituloDestaque>Web Developer</STituloDestaque>
+          <SContexto>
+            Conquiste sua liberdade financeira e entre para o mundo dos
+            investimentos com os maiores especialistas do mercado.
+          </SContexto>
+          <SBotaoContact>Contact</SBotaoContact>
+          <SContexto>
+            Entre em contato pelo <span>Email</span>
+          </SContexto>
+        </div>
+        <SImagePerfil
+          src="https://media.licdn.com/dms/image/D4D03AQEcxYXuL6pzHg/profile-displayphoto-shrink_800_800/0/1692718684966?e=1707350400&v=beta&t=QlRX4PRmwMmwkbidOPuXSRCShmATHhk5tDTY9GE1fHQ"
+          alt=""
+        />
+      </SSessaoHome>
     </>
   )
 }
--- src/containers/Home/style.ts
+++ src/containers/Home/style.ts
@@ -8,7 +8,7 @@ export const SHeader = styled.header`
   position: sticky;
   top: 0px;
   left: 0px;
-  background-color: rgba(0, 0, 0, 0.8);
+  background-color: #2a2a2a;
   border-radius: 0px 0px 16px 16px;
   padding: 8px 32px;
   color: #fff;
@@ -27,4 +27,67 @@ export const SNavBar = styled.ul`
       padding: 4px;
     }
   }
+`
+
+export const STextoDestaque = styled.p`
+  color: #4ee;
+  font-weight: bold;
+  margin-bottom: -24px;
+`
+
+export const SContexto = styled.p`
+  color: #ddd;
+  max-width: 80%;
+
+  span {
+    color: #ffffff;
+    font-weight: bold;
+  }
+`
+
+export const STituloDestaque = styled.h2`
+  color: white;
+  font-size: 40px;
+`
+
+export const SBotaoContact = styled.button`
+  color: #003;
+  padding: 16px;
+  width: 50%;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  border-radius: 8px;
+  border: none;
+  background-color: #00e6f9;
+  transition: all ease 0.5s;
+  margin-bottom: -16px;
+
+  &:hover {
+    box-shadow: 0px 0px 24px 8px rgba(0, 230, 249, 0.7);
+  }
+`
+
+export const SSessaoHome = styled.section`
+  width: 100%;
+  min-height: 600px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div {
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: left;
+    gap: 24px;
+    margin-left: 16px;
+  }
+`
+
+export const SImagePerfil = styled.img`
+  border-radius: 50%;
+  max-width: 300px;
+  height: 300px;
 `
