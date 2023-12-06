ba6b564eb1fd4fa1941e3523a0274836eb0c5b31
Tallyson Freitas <tallyson.eu@outlook.com>
2023-12-06 12:20:01 -0300

    instalando routerDOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import PaginaDeCadastro from './containers/PaginaDeCadastro'
 import { EstiloGlobal } from './style'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
+  const rotas = createBrowserRouter([
+    { path: '/', element: <PaginaDeCadastro /> },
+    {
+      path: 'home',
+      element: (
+        <div>
+          <h1>ola mundo</h1>
+        </div>
+      )
+    }
+  ])
+
   return (
-    <>
+    <Provider store={store}>
       <EstiloGlobal />
-      <PaginaDeCadastro />
-    </>
+      <RouterProvider router={rotas} />
+    </Provider>
   )
 }
 
